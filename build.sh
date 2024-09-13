4a9ce46272036237ef30d2cdc2976644d2c76c67
Ron Burk <ronburk@gmail.com>
2024-09-12 19:26:26 -0700

    sync

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ CFLAGS="-Wall -Wextra -fmax-errors=2"
 g++ -o cstr cstr.cpp $CFLAGS
 ./cstr blud.lua builtin.blud >./bludlua.c
 
-gcc -o blud blud.c bludlua.c oslinux.c -I/usr/local/include/luajit-2.1 $LDFLAGS $CFLAGS
+gcc -MMD -MP -o blud blud.c bludlua.c oslinux.c -I/usr/local/include/luajit-2.1 $LDFLAGS $CFLAGS
 exit 0
 
 g++ -o blud blud.cpp luawrap.cpp -I/usr/local/include/luajit-2.1 $LDFLAGS $CFLAGS
