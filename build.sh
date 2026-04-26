2d6eebd46f2ee74ee9b8369eee7bed9f230c36b3
Ron Burk <ronburk@gmail.com>
2026-04-25 22:17:30 -0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
-LDFLAGS="-L./luajit/src -lluajit"
+LUAJIT_FLAGS="$(pkg-config --cflags --libs luajit)"
+#LDFLAGS="-L./luajit/src -lluajit"
 CFLAGS="-Wall -Wextra -fmax-errors=2"
 g++ -o cstr cstr.cpp $CFLAGS
 ./cstr main.lua init.lua blud.lua builtin.blud debug.lua implicit.lua >./bludlua.c
 
-gcc -MMD -MP -o blud blud.c bludlua.c oslinux.c -I/usr/local/include/luajit-2.1 $LDFLAGS $CFLAGS
+gcc -MMD -MP -o blud blud.c bludlua.c oslinux.c $LUAJIT_FLAGS $CFLAGS
 exit 0
 
-g++ -o blud blud.cpp luawrap.cpp -I/usr/local/include/luajit-2.1 $LDFLAGS $CFLAGS
+g++ -o blud blud.cpp luawrap.cpp $LUAJIT_FLAGS $CFLAGS
