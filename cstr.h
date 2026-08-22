374696507eff936f0f49dcaa43e61baf50560bae
ronburk <ronburk@gmail.com>
2026-08-22 00:29:15 -0700

    Declare compiled cstr lookup

--- cstr.h
+++ cstr.h
@@ -1,11 +1,14 @@
 #ifndef CSTR_H_
 #define CSTR_H_
 
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 extern char* CSTRGet(const char* filename);
+extern const char* CSTRGetCompiled(const char* filename, size_t* size);
 
 #ifdef __cplusplus
 }
